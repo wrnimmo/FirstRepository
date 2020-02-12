a71b76db06ec24cafd148fa42d8d6c2dbf5e2d2b
Wesley Nimmo <wrnimmo@uark.edu>
2020-02-11 23:12:43 -0600

    Task 2. Changes to be more inclusive

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,8 +2,8 @@ class HelloWorldApp
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
-        System.out.println("Says George.");     //Task 1 - Display who is saying hello.
+        System.out.println("Hello Universe!");      //Task 2 - Be more inclusive.
+        System.out.println("From Earth.");          //Task 2 - Be more inclusive.
     }
 
 }
