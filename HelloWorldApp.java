399681caa524831ceee47dc08ac75fa8d8bae9bd
Wesley Nimmo <wrnimmo@uark.edu>
2020-02-11 22:58:47 -0600

    Add comment to task 1

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,7 +3,7 @@ class HelloWorldApp
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
-        System.out.println(“Says George.”);
+        System.out.println("Says George.");     //Task 1 - Display who is saying hello.
     }
 
 }
